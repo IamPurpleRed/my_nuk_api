924e53dee2d6f4da679520114ce3d5c0c41bb336
PurpleRed <benson060211@gmail.com>
2021-08-25 18:02:14 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+My NUK
+===
+###### tags: `GitHub`, `Node.JS`
+
+## 簡介 | Introduction
+一個簡易的API提供給 my_nuk Flutter APP 使用。
+
+A simple API for my_nuk Flutter APP.
+
+## 路由 | Routes
+- verification (驗證在APP上的登入資訊是否正確)
+- user (提供給使用者的所有功能)
+  - grades (查詢歷年學期成績和排名)
+  - schedule (查詢本學期課表)
+
+## 版本歷程紀錄 | Version History
---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var app = express();
+app.use(express.static('./public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res) => {
+    res.status(404).send('404 Not Found.');
+});
+
+app.use((err, req, res) => {
+    console.log(err);
+    res.status(500).send('Internal server error.');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port);
