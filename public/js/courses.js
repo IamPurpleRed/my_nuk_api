9b4f9430dca7e98b71c779aa0e6c1bad5e0bfd36
YU-HSIU <a1085524@mail.nuk.edu.tw>
2021-09-03 22:48:07 +0800

    feat: 建立表格+賦予ID(透過EJS)

--- public/js/courses.js
+++ public/js/courses.js
@@ -1,1 +1,19 @@
-console.log('hello');
+// let loginId = 'a1085524';
+// axios.get("http://localhost:3000/api/courses",{
+//         params:{
+//             id:loginId,
+//             channel:1
+//         }
+//     })
+//     .then(function(response){
+//     if(response.request.readyState === 4 && response.status === 200){
+//         console.log(response.data);
+//     } else{
+//         console.log("error");
+//     }
+// });
+let test = document.getElementById("test");
+console.log(test);
+//格式: 1-1-1: Monday First-Course Course-name
+//          2: Monday Second-Course Classroom
+//          3: Monday Third-Course Professor
