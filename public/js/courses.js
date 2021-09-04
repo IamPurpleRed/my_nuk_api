e777d329dc7fc33365aa6fd55157149dddea114a
YU-HSIU <a1085524@mail.nuk.edu.tw>
2021-09-04 10:47:40 +0800

    feat: 將JSON內容放進表格之中

--- public/js/courses.js
+++ public/js/courses.js
@@ -1,19 +1,37 @@
-// let loginId = 'a1085524';
-// axios.get("http://localhost:3000/api/courses",{
-//         params:{
-//             id:loginId,
-//             channel:1
-//         }
-//     })
-//     .then(function(response){
-//     if(response.request.readyState === 4 && response.status === 200){
-//         console.log(response.data);
-//     } else{
-//         console.log("error");
-//     }
-// });
-let test = document.getElementById("test");
-console.log(test);
-//格式: 1-1-1: Monday First-Course Course-name
-//          2: Monday Second-Course Classroom
-//          3: Monday Third-Course Professor
+let loginId = 'a1085524';
+axios.get("http://localhost:3000/api/courses",{
+        params:{
+            id:loginId,
+            channel:1
+        }
+    })
+    .then(function(response){
+    if(response.request.readyState === 4 && response.status === 200){
+        let info = response.data;
+        for(let i in info){
+            let name = info[i][1];          //取出課程名稱
+            let lecturer = info[i][2];      //取出教授
+            let place = info[i][3];         //取出地點
+            let temp = new Object();
+            for(let j in info[i][2]){
+               if(info[i][1][j] != null){
+                   let course = Number(j);
+                   course++;                //JSON課程陣列從0開始 介面表格從1開始 因此加1
+                   temp = document.getElementById(course+'-'+i+'-1');
+                   temp.textContent = info[i][1][j];
+                   temp = document.getElementById(course+'-'+i+'-3');
+                   temp.textContent = info[i][2][j];
+                   temp = document.getElementById(course+'-'+i+'-2');
+                   temp.textContent = info[i][3][j];
+               }
+            }
+            delete temp;
+        }
+        
+    } else{
+        console.log("error");
+    }
+});
+//格式: 1-1-1: Monday First-Course 課程名稱
+//          2: Monday Second-Course 教室地點
+//          3: Monday Third-Course 教授名稱
