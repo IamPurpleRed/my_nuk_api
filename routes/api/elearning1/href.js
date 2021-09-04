7a9bdc3c3310887e12173b2e60b3b1867d1224e7
PurpleRed <benson060211@gmail.com>
2021-09-04 16:22:45 +0800

    fixed: e平台爬蟲漏洞

--- routes/api/elearning1/href.js
+++ routes/api/elearning1/href.js
@@ -1,83 +1,93 @@
 let express = require('express');
-var request = require('request');
-var iconv = require('iconv-lite');
-var cheerio = require('cheerio');
+let request = require('request');
+let rp = require('request-promise');
+let iconv = require('iconv-lite');
+let cheerio = require('cheerio');
 
 let router = express.Router();
 
-router.post('/', (req, res) => {
-    let id = req.body.id;
+router.get('/', (req, res) => {
+    let id = req.query.id;
+    let jar = request.jar();
 
+    let waitingToCrawl = [];  // 若該使用者的課程頁數有n頁，則此陣列會有n-1項
     let finalResult = {
         'status': 'success',
         'content': [],
     };
-    let page = 0; // 目前爬到第幾頁
-    let status = true; // 判斷是否要繼續爬蟲
-    let requestHeaders = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36',
-        'Content-Type': 'application/x-www-form-urlencoded'
-    };
 
-    let elearning1 = request.defaults({ jar: true }); // cookie開啟
+    let elearning1 = request.defaults({ jar: jar }); // cookie開啟
     elearning1({
         url: 'http://elearning.nuk.edu.tw/p_login_stu_at.php?bStu_id=' + id,
         method: 'GET',
         encoding: null,
-        headers: requestHeaders
     }, (err, res, body) => {
-        /* 如果偵測到錯誤或是收到的body為空 */
-        if (err || !body) {
+        if (err) {
             fail('伺服器錯誤', 'Failed to login e-learning 1.0. Error message: ' + err);
+        } else if (!body) {
+            fail('伺服器錯誤', 'Failed to login e-learning 1.0. Error message: Body is null.');
         } else {
-            let cookie;
-            try {
-                cookie = res.headers['set-cookie'][0];  // sessionID
-            } catch (err) {
-                fail('伺服器錯誤', 'Failed to login e-learning 1.0. Error message: ' + err);
-            }
-            asyncTask(cookie);
+            firstPage();
         }
     });
 
-    async function asyncTask(cookie) {
-        /* 爬蟲直到status為false */
-        while (status) {
-            page++;
-            await currentPage(cookie);
-        }
-        showPage(JSON.stringify(finalResult, null, 2));
+    /* INFO: 造訪第一頁，取得總頁數 & 第一頁的爬蟲 */
+    function firstPage() {
+        let firstPageRequest = rp.defaults({ jar: jar });
+        firstPageRequest({
+            url: 'http://elearning.nuk.edu.tw/m_student/m_stu_index.php?p=1',
+            method: 'GET',
+            encoding: null,
+            transform: (body) => {
+                let result = iconv.decode(body, 'utf-8');
+                return cheerio.load(result);
+            }
+        }).then(($) => {
+            loopItems($); // 第一頁課程內容
+            let page = $('#MMpage').children().length - 4;  // 總共要爬的頁數(包括第一頁)
+
+            /* 取得頁數後，開始加準備要爬的網址到waitingToCrawl陣列 */
+            for (let i = 2; i <= page; i++) {
+                waitingToCrawl.push('http://elearning.nuk.edu.tw/m_student/m_stu_index.php?p=' + i);
+            }
+
+            return page;
+        }).then((page) => {
+            waitingToCrawl.reduce((p, item) => {
+                return p.then(() => {
+                    return gotoPage(item);
+                });
+            }, Promise.resolve()).then(() => {
+                showPage(JSON.stringify(finalResult, null, 2));
+            });
+        });
     }
 
-    function currentPage(cookie) {
-        return new Promise((resolve, reject) => {
-            let pageRequest = request.defaults({ jar: true }); // cookie開啟
+    /* INFO: 爬蟲程式碼，每一頁最多可以顯示21頁課程 */
+    function loopItems($) {
+        for (let i = 1; i <= 21; i++) {
+            let item = $('#myTable > tbody > tr:nth-child(' + i + ') > td:nth-child(2) > a');
+            if (item.text() == '') {
+                break;
+            } else {
+                finalResult.content.push([item.text(), 'http://elearning.nuk.edu.tw' + item.attr('href').substring(2)]);
+            }
+        }
+    }
 
-            pageRequest({
-                url: 'http://elearning.nuk.edu.tw/m_student/m_stu_index.php?p=' + page,
-                method: 'GET',
-                encoding: null,
-                headers: requestHeaders,
-                Cookie: cookie
-            }, (err, res, body) => {
-                if (err || !body) {
-                    status = false;
-                    resolve('fail');
-                } else {
-                    let result = iconv.decode(body, 'utf-8');
-                    let $ = cheerio.load(result);
-                    for (let i = 1; i <= 21; i++) {
-                        let item = $('#myTable > tbody > tr:nth-child(' + i + ') > td:nth-child(2) > a');
-                        if (item.text() == '') {
-                            status = false;
-                            break;
-                        } else {
-                            finalResult.content.push([item.text(), 'http://elearning.nuk.edu.tw' + item.attr('href').substring(2)]);
-                        }
-                    }
-                    resolve('finish page ' + page);
-                }
-            });
+    /* INFO: 發出request的函式，所有在waitingToCrawl陣列內的網址都會在這裡發出請求 */
+    function gotoPage(url) {
+        let pageCall = rp.defaults({ jar: jar });
+        return pageCall({
+            url: url,
+            method: 'GET',
+            encoding: null,
+            transform: (body) => {
+                let result = iconv.decode(body, 'utf-8');
+                return cheerio.load(result);
+            }
+        }).then(($) => {
+            loopItems($);
         });
     }
 
