36409c12b556450ba48ce5cf21aad2c06284e573
PurpleRed <benson060211@gmail.com>
2021-09-04 12:31:46 +0800

    fixed: api/courses(e平台1.0通道) 爬蟲漏洞 & 不爬教授名字

--- routes/api/courses.js
+++ routes/api/courses.js
@@ -7,112 +7,76 @@ let router = express.Router();
 
 router.get('/', (req, res) => {
     let id = req.query.id;
+    let pwd = req.query.pwd;
     let channel = req.query.channel;
-    let requestHeaders = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36',
-        'Content-Type': 'application/x-www-form-urlencoded'
-    };
+    let cookie;
 
-    /* 驗證管道1：E平台 1.0 */
-    if (channel == 1) {
-        let elearning1 = request.defaults({ jar: true }); // cookie開啟
-        elearning1({
+    /* INFO: 驗證管道2：E平台 1.0(考量效率，此管道不會爬教授名字) */
+    if (channel == 2) {
+        let jar = request.jar();
+        let request1 = request.defaults({ jar: jar });
+        request1({
             url: 'http://elearning.nuk.edu.tw/p_login_stu_at.php?bStu_id=' + id,
             method: 'GET',
             encoding: null,
-            headers: requestHeaders
         }, (err, res, body) => {
-            /* 如果偵測到錯誤或是收到的body為空 */
-            if (err || !body) {
+            if (err) {
                 fail('伺服器錯誤', 'Failed to login e-learning 1.0. Error message: ' + err);
+            } else if (!body) {
+                fail('伺服器錯誤', 'Failed to login e-learning 1.0. Error message: Body is null.');
             } else {
-                let cookie = res.headers['set-cookie'][0];  // sessionID
+                try {
+                    cookie = res.headers['set-cookie'][0];  // sessionID
+                } catch (err) {
+                    fail('伺服器錯誤', 'Failed to login e-learning 1.0. Error message: ' + err);
+                }
                 gotoHomepage(cookie);
             }
         });
 
         function gotoHomepage(cookie) {
-            let homepageRequest = request.defaults({ jar: true }); // cookie開啟
-            homepageRequest({
+            let request2 = request.defaults({ jar: jar });
+            request2({
                 url: 'http://elearning.nuk.edu.tw/m_student/m_stu_index.php',
                 method: 'GET',
                 encoding: null,
-                headers: requestHeaders,
                 Cookie: cookie
             }, (err, res, body) => {
-                if (err || !body) {
-                    fail('伺服器錯誤', 'Failed to get to e-learning 1.0 homepage. Error message: ' + err);
+                if (err) {
+                    fail('伺服器錯誤', 'Failed to GET e-learning 1.0 homepage. Error message: ' + err);
+                } else if (!body) {
+                    fail('伺服器錯誤', 'Failed to GET e-learning 1.0 homepage. Error message: Body is null.');
                 } else {
                     let result = iconv.decode(body, 'utf-8');
                     let $ = cheerio.load(result);
                     
-                    asyncTask($, cookie);
-                    // NOTE: 由於E平台1.0 sessionID的機制，爬每個課程的網址都需要先等待上個工作完成，因此需要使用非同步函式搭配await，包住接下來的所有工作
-                }
-            });
-        }
-        async function asyncTask($, cookie) {
-            let data = [];  // 爬下來但未整理的資料
+                    /* 爬蟲工作 */
+                    let data = [];  // 存放爬下來但未整理的資料
+                    for (let i = 1; true; i++) {
+                        let selector = '#myTable > tbody > tr:nth-child(' + i + ')';
+                        let item = [];
 
-            for (let i = 1; true; i++) {
-                let selector = '#myTable > tbody > tr:nth-child(' + i + ')';
-                let item = [];
+                        /* 若該區塊非粉紅色，就不是本學期課程 */
+                        if ($(selector + ' > td:nth-child(1)').attr('bgcolor') != '#FFDCD7') {
+                            break;
+                        }
 
-                /* 若該區塊非粉紅色，就不是本學期課程 */
-                if ($('#myTable > tbody > tr:nth-child(' + i + ') > td:nth-child(1)').attr('bgcolor') != '#FFDCD7') {
-                    break;
-                }
+                        let id = $(selector + ' > td:nth-child(1)').text();
+                        let name = $(selector + ' > td:nth-child(2) > a').text();
+                        let classroom = $(selector + ' > td:nth-child(5)').text();
+                        let time = $(selector + ' > td:nth-child(6)').text();
+                        item.push(id);
+                        item.push(name);
+                        item.push(classroom);
+                        item.push(time);
+                        item.push('');  // 空的教授名字
 
-                let id = $(selector + ' > td:nth-child(1)').text();
-                let name = $(selector + ' > td:nth-child(2) > a').text();
-                let classroom = $(selector + ' > td:nth-child(5)').text();
-                let time = $(selector + ' > td:nth-child(6)').text();
-                let href = 'http://elearning.nuk.edu.tw/' + $(selector + ' > td:nth-child(2) > a').attr('href').substring(3);
-                await getProfName(href, cookie).then((profName) => {
-                    item.push(id);
-                    item.push(name);
-                    item.push(classroom);
-                    item.push(time);
-                    item.push(profName);
-                    data.push(item);
-                });
-            }
-                    
-            let finalResult = formatData(data);
-            showPage(JSON.stringify(finalResult, null, 2));
-        }
-
-        function getProfName(href, cookie) {
-            return new Promise((resolve, reject) => {
-                let coursepageRequest = request.defaults({ jar: true }); // cookie開啟
-                coursepageRequest({
-                    url: href,
-                    method: 'GET',
-                    encoding: null,
-                    headers: requestHeaders,
-                    Cookie: cookie
-                }, (err, res, body) => {
-                    if (err || !body) {
-                        resolve('(無法取得教授名字)');
-                    } else {
-                        let coursepageRequest2 = request.defaults({ jar: true });
-                        coursepageRequest2({
-                            url: 'http://elearning.nuk.edu.tw/m_mtp/m_mpt_p1.php',
-                            method: 'GET',
-                            encoding: null,
-                            headers: requestHeaders,
-                            Cookie: cookie
-                        }, (err, res, body) => {
-                            if (err || !body) {
-                                resolve('(無法取得教授名字)');
-                            } else {
-                                let result = iconv.decode(body, 'utf-8');
-                                let $ = cheerio.load(result);
-                                resolve($('#form1 > table > tbody > tr:nth-child(2) > td > table > tbody > tr:nth-child(2) > td.txt_black_BU > table > tbody > tr:nth-child(6) > td:nth-child(2)').text());
-                            }
-                        });
+                        data.push(item);
                     }
-                });
+
+                    let finalResult = formatData(data);  // 進行資料整理
+                    showPage(JSON.stringify(finalResult, null, 2));
+                }
             });
         }
 
