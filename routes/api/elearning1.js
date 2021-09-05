6cad9877bbaec96cedd8009864af0f634b4a5edf
PurpleRed <benson060211@gmail.com>
2021-09-05 12:29:26 +0800

    feat: api/elearning1/handouts 完成

--- routes/api/elearning1.js
+++ routes/api/elearning1.js
@@ -5,4 +5,7 @@ let router = express.Router();
 let href = require('./elearning1/href');
 router.use('/href', href);
 
+let handouts = require('./elearning1/handouts');
+router.use('/handouts', handouts);
+
 module.exports = router;
