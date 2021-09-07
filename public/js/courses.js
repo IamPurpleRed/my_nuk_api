e7fc794561f9f24f0ca10c4d75e5b8f517dfc0ae
DESKTOP-BENCHEN\Ben-Chen <boyben001@gmail.com>
2021-09-07 15:21:24 +0800

    fixed: 修改左右拖曳以及時間欄固定

--- public/js/courses.js
+++ public/js/courses.js
@@ -5,50 +5,57 @@ let weekends = document.getElementById("courseForWeekend");
 let weekends_title = document.getElementById("weekend");
 let weekdays_title = document.getElementById("weekday");
 let isWeekDay = true;
-function tran(){
-    if(isWeekDay){
+let widthOfWrap = document.getElementById("Wrap");
+
+function tran() {
+    if (isWeekDay) {
         weekdays.style.opacity = 0;
         weekdays_title.style.opacity = 0;
         weekends.style.opacity = 1;
         weekends_title.style.opacity = 1;
-    }else{
+        weekends_title.style.zIndex = 5;
+        weekends.style.zIndex = 4;
+    } else {
         weekdays.style.opacity = 1;
         weekdays_title.style.opacity = 1;
         weekends.style.opacity = 0;
         weekends_title.style.opacity = 0;
+        widthOfWrap.style.width = 800 + "px";
+        weekends_title.style.zIndex = 2;
+        weekends.style.zIndex = 2;
     }
     isWeekDay = !isWeekDay;
+
 }
-axios.get("http://localhost:3000/api/courses",{
-        params:{
-            id:loginId,
-            channel:2
+axios.get("http://localhost:3000/api/courses", {
+        params: {
+            id: loginId,
+            channel: 2
         }
     })
-    .then(function(response){
-    if(response.request.readyState === 4 && response.status === 200){
-        let info = response.data;
-        for(let i in info){
-            let name = info[i][1];          //取出課程名稱
-            let lecturer = info[i][2];      //取出教授
-            let place = info[i][3];         //取出地點
-            let temp = new Object();
-            for(let j in info[i][2]){
-               if(info[i][1][j] != null){
-                   let course = Number(j);
-                   course++;                //JSON課程陣列從0開始 介面表格從1開始 因此加1
-                   temp = document.getElementById(course+'-'+i+'-1');
-                   temp.textContent = info[i][1][j];
-                //    temp = document.getElementById(course+'-'+i+'-3');
-                //    temp.textContent = info[i][2][j];
-                   temp = document.getElementById(course+'-'+i+'-2');
-                   temp.textContent = info[i][3][j];
-               }
+    .then(function (response) {
+        if (response.request.readyState === 4 && response.status === 200) {
+            let info = response.data;
+            for (let i in info) {
+                let name = info[i][1]; //取出課程名稱
+                let lecturer = info[i][2]; //取出教授
+                let place = info[i][3]; //取出地點
+                let temp = new Object();
+                for (let j in info[i][2]) {
+                    if (info[i][1][j] != null) {
+                        let course = Number(j);
+                        course++; //JSON課程陣列從0開始 介面表格從1開始 因此加1
+                        temp = document.getElementById(course + '-' + i + '-1');
+                        temp.textContent = info[i][1][j];
+                        //    temp = document.getElementById(course+'-'+i+'-3');
+                        //    temp.textContent = info[i][2][j];
+                        temp = document.getElementById(course + '-' + i + '-2');
+                        temp.textContent = info[i][3][j];
+                    }
+                }
+                delete temp;
             }
-            delete temp;         
+        } else {
+            console.log("error");
         }
-    } else{
-        console.log("error");
-    }
-});
-
+    });
