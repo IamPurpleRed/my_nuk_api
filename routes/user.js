a3875e007141add4e7b24b9c9383bc5903aaba6d
PurpleRed <benson060211@gmail.com>
2021-09-08 11:39:00 +0800

    feat: user/grades 路由初始化

--- routes/user.js
+++ routes/user.js
@@ -5,4 +5,7 @@ let router = express.Router();
 let courses = require('./user/courses');
 router.use('/courses', courses);
 
+let grades = require('./user/grades');
+router.use('/grades', grades);
+
 module.exports = router;
