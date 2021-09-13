e9dc45c95b273807de9c069c196f4983a1f2c7b7
DESKTOP-BENCHEN\Ben-Chen <boyben001@gmail.com>
2021-09-13 20:14:10 +0800

    fixed: 表格字數會跑版

--- public/js/grades.js
+++ public/js/grades.js
@@ -1,7 +1,7 @@
 let url = new URL(location.href);
 let loginId = url.searchParams.get('id');
 let loginPwd = url.searchParams.get('pwd');
-let admission = Number(loginId.slice(1,4));        //入學年度
+let admission = Number(loginId.slice(1, 4)); //入學年度
 let boolTerm = false;
 let boolSemester = false;
 let term = document.getElementById('Term');
@@ -9,51 +9,55 @@ let semester = document.getElementById('Semester');
 let courseTable = document.getElementById('courseTable');
 let res = null;
 axios.get('http://localhost:3000/api/grades', {
-    params: {
-        id: loginId,
-        pwd: loginPwd
-    }
-})
-.then(function (response) {
-    if (response.request.readyState === 4 && response.status === 200) {
-        res = response.data;
-    } else {
-        alert('Error: With Internet Problem');
-    }
-});
-term.addEventListener('change',changeTerm);
-semester.addEventListener('change',changeSemester);
-function changeTerm(){
+        params: {
+            id: loginId,
+            pwd: loginPwd
+        }
+    })
+    .then(function (response) {
+        if (response.request.readyState === 4 && response.status === 200) {
+            res = response.data;
+        } else {
+            alert('Error: With Internet Problem');
+        }
+    });
+term.addEventListener('change', changeTerm);
+semester.addEventListener('change', changeSemester);
+
+function changeTerm() {
     boolTerm = true;
     getData();
 }
-function changeSemester(){
+
+function changeSemester() {
     boolSemester = true;
     getData();
 }
-function getData(){
-    if(boolSemester && boolTerm){
-        let search_index = 2*(Number(semester.value) - admission)+Number(term.value)-1;
-        if(res.grades[search_index] != undefined){
-            courseTable.innerHTML = null;               //清空Table
-            showTitle();                                //Table加上標題
-            showData(search_index);                     //Table加上查詢內容
+
+function getData() {
+    if (boolSemester && boolTerm) {
+        let search_index = 2 * (Number(semester.value) - admission) + Number(term.value) - 1;
+        if (res.grades[search_index] != undefined) {
+            courseTable.innerHTML = null; //清空Table
+            showTitle(); //Table加上標題
+            showData(search_index); //Table加上查詢內容
         } else
             alert('Error: Wrong information.')
     }
 };
 //動態產生年度選項
-for(let i=admission;i<admission+5;i++){
+for (let i = admission; i < admission + 5; i++) {
     let option = document.createElement('option');
-    option.setAttribute('value',i);
+    option.setAttribute('value', i);
     option.textContent = i;
     semester.appendChild(option);
 }
-function showData(search_index){
+
+function showData(search_index) {
     let course = res.grades[search_index].subjects;
     let courseNum = res.grades[search_index].subjects.length;
     let tbody = document.createElement('tbody');
-    for(let i in course){
+    for (let i in course) {
         let tr = document.createElement('tr');
         let tdCode = document.createElement('td');
         let tdName = document.createElement('td');
@@ -61,6 +65,8 @@ function showData(search_index){
         tdCode.textContent = course[i].id;
         tdName.textContent = course[i].name;
         tdScore.textContent = course[i].score;
+        tdCode.setAttribute('class', 'wid-90');
+        tdScore.setAttribute('class', 'wid-60');
         tr.appendChild(tdCode);
         tr.appendChild(tdName);
         tr.appendChild(tdScore);
@@ -68,7 +74,8 @@ function showData(search_index){
     }
     courseTable.appendChild(tbody);
 }
-function showTitle(){
+
+function showTitle() {
     let thead = document.createElement('thead');
     let tr = document.createElement('tr');
     let code = document.createElement('td');
@@ -76,11 +83,11 @@ function showTitle(){
     let score = document.createElement('td');
     code.textContent = '課號';
     name.textContent = '課程名稱';
-    score.textContent = '學期成績';
+    score.textContent = '分數';
     tr.appendChild(code);
     tr.appendChild(name);
     tr.appendChild(score);
     thead.appendChild(tr);
-    thead.setAttribute('class','table-dark');
+    thead.setAttribute('class', 'table-dark');
     courseTable.appendChild(thead);
 }
